e5467ea8d6174fa96ecfb8b5da72c825a23d3538
Abay Sevi <abaysevi012@gmail.com>
2022-08-16 09:58:10 +0530

    progrsss

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function yea() {
+
+    a = document.getElementById("file")
+    console.log(a.value)
+    if (a.value == 20) {
+        document.getElementById("demo").innerHTML = "yooo this is done";
+    }
+    else {
+        a.value = a.value + 1
+
+    }
+
+    // console.log("sdfsdf")
+}
+
