25307ecf6cde2a150bdb727777ec62ceb98e0a37
Abay Sevi <abaysevi012@gmail.com>
2022-08-17 11:35:51 +0530

    yah added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function yea() {
     a = document.getElementById("file")
     console.log(a.value)
     if (a.value == 20) {
-        document.getElementById("demo").innerHTML = "yooo this is done";
+        document.getElementById("demo").innerHTML = "yooo this is done YEaaa";
     }
     else {
         a.value = a.value + 1
