92c2c361bc48a311d4b303e154fdafdeb2c0b37d
Abay Sevi <abaysevi012@gmail.com>
2023-01-15 11:51:56 +0530

    yo thi is coo

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
 
-function yea() {
-
-    a = document.getElementById("file")
-    console.log(a.value)
-    if (a.value == 20) {
-        document.getElementById("demo").innerHTML = "reached limit";
-    }
-    else {
-        a.value = a.value + 1
-
-    }
-
-    // console.log("sdfsdf")
-}
-
